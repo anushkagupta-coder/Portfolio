f671fa8be924db2613b1960d7511375bd819b8ff
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-06 15:23:25 +0530

    home frontend and css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,15 +3,17 @@ import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <section id="home" style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center", background: "linear-gradient(135deg, #6C63FF, #FF6584)", color: "white", textAlign: "center", padding: "0 20px" }}>
+    <section id="home" style={{ minHeight: "100vh", display: "flex", alignItems: "center", flexDirection: "column", 
+    justifyContent: "flex-start", background: "linear-gradient(135deg, #eb60e2ff, #4d0747ff)", 
+    color: "white", textAlign: "center", padding: "0px 20px", }}>
       
       <div>
-        <h1 style={{ fontSize: "3rem", marginBottom: "10px" }}>Hi, I'm Anushka Gupta 👋</h1>
-        <p style={{ fontSize: "1.2rem", marginBottom: "20px" }}>I'm a Computer Engineering student </p>
+        <h1 style={{ fontSize: "2.5rem", marginBottom: "10px" }}>Hi, I'm Anushka Gupta</h1>
+        <p style={{ fontSize: "1.7rem", marginBottom: "20px" }}>I'm a Computer Engineering student </p>
         
         {/* Buttons or links to projects/contact */}
         <div style={{ marginBottom: "20px" }}>
-          <a href="projects" style={{ marginRight: "10px", padding: "10px 20px", borderRadius: "8px", backgroundColor: "white", color: "#6C63FF", fontWeight: "bold", textDecoration: "none", transition: "0.3s" }}>My Projects</a>
+          <a href="projects" style={{ marginRight: "10px", padding: "10px 20px", borderRadius: "8px", backgroundColor: "white", color: "#101278ff", fontWeight: "bold", textDecoration: "none", transition: "0.3s" }}>My Projects</a>
           <a href="contact" style={{ padding: "10px 20px", borderRadius: "8px", border: "2px solid white", color: "white", fontWeight: "bold", textDecoration: "none", transition: "0.3s" }}>Contact Me</a>
         </div>
 
@@ -21,6 +23,18 @@ const Home = () => {
           <a href="https://www.linkedin.com/in/anushka-gupta-aa9b40286/" target="_blank" rel="noreferrer"><FaLinkedin /></a>
           <a href="anushka612004@gmail.com"><FaEnvelope /></a>
         </div>
+
+        <div className="home-card">
+            <h2>3rd Year student </h2>
+            <p>Full-stack developer | React | Node.js | MySQL,MongoDB |Git  </p>
+        </div>
+
+        <div className="right-card">
+            <h2>CGPA - 9.62 </h2>
+            <p>2027 Graduate Looking for industrial experience 
+                and exposure in IT industry </p>
+        </div>
+
       </div>
 
     </section>
