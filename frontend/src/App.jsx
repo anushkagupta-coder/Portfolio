e51c9b31eee47ae9c5c388bb299177588237ad54
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-10-06 14:24:18 +0530

    Pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/about" element={<div>About Page</div>} />
+        <Route path="/contact" element={<div>Contact Page</div>} />
+      </Routes>
+    </Router>
   )
 }
 
